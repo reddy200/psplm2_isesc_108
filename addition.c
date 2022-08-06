c4080f5ea3b32255064e5f0ac767253adc49e40b
reddy200 <110719351+reddy200@users.noreply.github.com>
2022-08-06 16:55:03 +0530

    addition creating a function

--- addition.c
+++ addition.c
@@ -12,6 +12,6 @@ int main()
     printf("enter two numbers to be added: ");
     scanf("%d %d", &a, &b);
     s= addition(a,b);
-    printf("the sum is %d",s);
+    printf("the sum of %d and %d is %d",a,b,s);
     return 0;
 }
