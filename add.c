4028cec4570c107c5ccd028abe6927ba4c33006d
reddy200 <110719351+reddy200@users.noreply.github.com>
2022-08-06 16:37:43 +0530

    addition of two numbers

---
+++ add.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,s ;
+    printf("enter two numbers to be added: ");
+    scanf("%d %d", &a, &b);
+    s= a+b;
+    printf("the sum of %d and %d is %d", a,b,s);
+    return 0;
+}
+  
